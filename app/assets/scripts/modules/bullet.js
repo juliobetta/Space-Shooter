bd6fbbca69e35a6509e811f8fc3fa8e2f3332a5a
Julio Betta <juliobetta@gmail.com>
2014-12-21 00:11:48 -0200

    Add basic shooting

---
+++ app/assets/scripts/modules/bullet.js
@@ -0,0 +1,64 @@
+Module('Shoter.Bullet', function(Bullet) {
+  'use strict';
+
+
+  /**
+   * Initialize
+   */
+  Bullet.fn.initialize = function() {
+    this.bullets = GAME.add.group();
+    this.addProperties();
+  };
+
+
+  /**
+   * Add bullets properties
+   */
+  Bullet.fn.addProperties = function() {
+    this.bullets.enableBody = true;
+    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    this.bullets.createMultiple(30, 'bullet');
+    this.bullets.setAll('anchor.x', 0.5);
+    this.bullets.setAll('anchor.y', 1);
+    this.bullets.setAll('outOfBoundsKill', true);
+    this.bullets.setAll('checkWorldBounds', true);
+  };
+
+
+  /**
+   * Prepare bullets to be fired
+   */
+  Bullet.fn.prepareBullets = function() {
+    console.log('firebutton hit');
+    EventBus.dispatch('bullets-ready', Bullet.fn, this.bullets);
+  };
+
+
+  /**
+   * ########################################################################################
+   * Event Listeners ########################################################################
+   * ########################################################################################
+  */
+
+  Bullet.fn.bindEvents = function() {
+    EventBus.addEventListener('firebutton-hit', this.prepareBullets, Bullet.fn);
+  };
+
+
+  /**
+   * ########################################################################################
+   * Main states ############################################################################
+   * ########################################################################################
+  */
+
+  Bullet.fn.create = function() {
+    this.initialize();
+    this.bindEvents();
+  };
+
+
+  // ########################################################################################
+  // ########################################################################################
+
+  Shooter.Application.addToCreate(Bullet.fn.create.bind(Bullet.fn));
+});
---
+++ app/assets/scripts/modules/ship-trail.js
@@ -0,0 +1,70 @@
+Module('Shooter.ShipTrail', function(ShipTrail) {
+  'use strict';
+
+  var initialized = false;
+
+  /**
+   * Initialize
+   * @param  {Object} ship
+   */
+  ShipTrail.fn.initializeWith = function(ship) {
+    this.shipTrail = GAME.add.emitter(ship.x, ship.y + 10, 400);
+    this.addProperties();
+    initialized = true;
+  };
+
+
+  /**
+   * Add ship trail properties
+   */
+  ShipTrail.fn.addProperties = function() {
+    this.shipTrail.width = 10;
+    this.shipTrail.makeParticles('bullet');
+    this.shipTrail.setXSpeed(30, -30);
+    this.shipTrail.setYSpeed(200, 180);
+    this.shipTrail.setRotation(50, -50);
+    this.shipTrail.setAlpha(1, 0.01, 800);
+    this.shipTrail.setScale(0.05, 0.4, 0.05, 0.4, 2000, Phaser.Easing.Quintic.Out);
+    this.shipTrail.start(false, 5000, 10);
+  };
+
+
+  /**
+   * Keep the shipTrail lined up with the ship
+   * @param  {Object} event
+   * @param  {Object} ship
+   */
+  ShipTrail.fn.updateCoordinates = function(event, ship) {
+    if(!initialized) this.initializeWith(ship);
+
+    this.shipTrail.x = ship.x;
+    this.shipTrail.y = ship.y;
+  };
+
+
+  /**
+   * ########################################################################################
+   * Event Listeners ########################################################################
+   * ########################################################################################
+  */
+
+  ShipTrail.fn.bindEvents = function() {
+    EventBus.addEventListener('ship-updated', this.updateCoordinates, ShipTrail.fn);
+  };
+
+
+  /**
+   * ########################################################################################
+   * Main states ############################################################################
+   * ########################################################################################
+  */
+
+  ShipTrail.fn.create = function() {
+    this.bindEvents();
+  };
+
+  // ########################################################################################
+  // ########################################################################################
+
+  Shooter.Application.addToCreate(ShipTrail.fn.create.bind(ShipTrail.fn));
+});
